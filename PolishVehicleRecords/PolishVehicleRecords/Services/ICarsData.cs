f82a9d3837b1df0d09d96601d2f2204ebf597c59
serasm <smigiel.sebastian@gmail.com>
2020-07-12 11:50:09 +0200

    Added factory for downloading cars data

---
+++ PolishVehicleRecords/PolishVehicleRecords/Services/ICarsData.cs
@@ -0,0 +1,11 @@
+﻿using PolishVehicleRecords.Models.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PolishVehicleRecords.Services
+{
+    interface ICarsData
+    {
+        public Task<List<Car>> GetCars(CarsSearch search);
+    }
+}
