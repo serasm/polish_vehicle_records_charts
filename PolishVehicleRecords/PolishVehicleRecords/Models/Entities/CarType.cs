2288da489046df93e8077517ba27cf3abe5f2bba
serasm <smigiel.sebastian@gmail.com>
2020-07-12 11:13:48 +0200

    Added class representation of type of car

---
+++ PolishVehicleRecords/PolishVehicleRecords/Models/Entities/CarType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolishVehicleRecords.Models.Entities
+{
+    public class CarType
+    {
+        public string Name { get; set; }
+        public bool IsSelected { get; set; } = false;
+    }
+}
