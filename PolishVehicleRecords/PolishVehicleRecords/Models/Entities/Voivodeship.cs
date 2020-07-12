a4fad61cc7f9e9c5691b43d6fc52c8d5a2a24b72
serasm <smigiel.sebastian@gmail.com>
2020-07-12 11:14:29 +0200

    Added class representation of voivodeship

---
+++ PolishVehicleRecords/PolishVehicleRecords/Models/Entities/Voivodeship.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolishVehicleRecords.Models.Entities
+{
+    public class Voivodeship
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public bool IsSelected { get; set; } = false;
+    }
+}
