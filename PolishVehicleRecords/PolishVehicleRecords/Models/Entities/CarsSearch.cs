e964aa830c447d662e9dc68726d68050e9c70f54
serasm <smigiel.sebastian@gmail.com>
2020-07-12 11:15:20 +0200

    Added model for searching a cars

---
+++ PolishVehicleRecords/PolishVehicleRecords/Models/Entities/CarsSearch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolishVehicleRecords.Models.Entities
+{
+    public class CarsSearch
+    {
+        public DateTime StartDate, EndDate;
+        public bool OnlyRegistered = true;
+        public ushort Limit = 100;
+        public List<string> Fields, Voivodeships, Types;
+    }
+}
