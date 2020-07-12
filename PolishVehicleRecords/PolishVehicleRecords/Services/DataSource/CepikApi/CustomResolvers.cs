e61094410e79e106a7ce50484851b28903212506
serasm <smigiel.sebastian@gmail.com>
2020-07-12 11:56:06 +0200

    Added custom resolvers to map json response to defined models

---
+++ PolishVehicleRecords/PolishVehicleRecords/Services/DataSource/CepikApi/CustomResolvers.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json.Serialization;
+using System.Collections.Generic;
+
+namespace PolishVehicleRecords.Services.DataSource.CepikApi
+{
+    public class CustomBaseResolver : DefaultContractResolver
+    {
+        public Dictionary<string, string> PropertyMappings { get; set; }
+
+        protected override string ResolvePropertyName(string propertyName)
+        {
+            string resolvedName = null;
+            var resolved = this.PropertyMappings.TryGetValue(propertyName, out resolvedName);
+            return (resolved) ? resolvedName : base.ResolvePropertyName(propertyName);
+        }
+    }
+
+    public class CustomCarResolver : CustomBaseResolver
+    {
+        public CustomCarResolver()
+        {
+            this.PropertyMappings = new Dictionary<string, string>
+            {
+                {"Brand", "marka" },
+                {"Model", "model" },
+                {"Type", "rodzaj-pojazdu" },
+                {"EngineCapacity", "pojemnosc-skokowa-silnika" },
+                {"Weight", "masa-wlasna" },
+                {"Fuel", "rodzaj-paliwa" },
+            };
+        }
+    }
+
+    public class CustomVoivedoshipResolver : CustomBaseResolver
+    {
+        public CustomVoivedoshipResolver()
+        {
+            this.PropertyMappings = new Dictionary<string, string>
+            {
+                {"Code", "klucz-slownika"},
+                {"Name", "wartosc-slownika"}
+            };
+        }
+    }
+
+    public class CustomCarTypeResolver : CustomBaseResolver
+    {
+        public CustomCarTypeResolver()
+        {
+            this.PropertyMappings = new Dictionary<string, string>
+            {
+                {"Name", "klucz-slownika"}
+            };
+        }
+    }
+}
