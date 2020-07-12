6b9acbaa6b3b0089a51c64cedf4ff0be6fceaeef
serasm <smigiel.sebastian@gmail.com>
2020-07-12 11:50:35 +0200

    Added factory for downloading dictionary values used in search

---
+++ PolishVehicleRecords/PolishVehicleRecords/Services/IDictionariesValues.cs
@@ -0,0 +1,12 @@
+﻿using PolishVehicleRecords.Models.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PolishVehicleRecords.Services
+{
+    interface IDictionariesValues
+    {
+        public Task<List<Voivodeship>> GetVoivodeships();
+        public Task<List<CarType>> GetCarTypes();
+    }
+}
