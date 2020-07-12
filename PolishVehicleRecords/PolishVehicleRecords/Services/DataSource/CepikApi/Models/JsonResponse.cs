21acc6f2eaa0b8fa17b4fb906b1e868178a7aef0
serasm <smigiel.sebastian@gmail.com>
2020-07-12 11:54:01 +0200

    Added model for Cepik API response

---
+++ PolishVehicleRecords/PolishVehicleRecords/Services/DataSource/CepikApi/Models/JsonResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace PolishVehicleRecords.Services.DataSource.CepikApi.Models
+{
+    public class JsonResponse<T>
+    {
+        public List<Data<T>> data { get; set; }
+    }
+    public class Data<T>
+    {
+        public T attributes { get; set; }
+    }
+    public class Records<T>
+    {
+        public T records { get; set; }
+    }
+}
